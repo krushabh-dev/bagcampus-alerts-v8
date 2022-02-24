c30fe5ba8fffa898a0d23ff8a0068d85387e7778
krushabh-dev <rushabhrbs@gmail.com>
2022-02-24 17:57:53 +0530

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import NavBar from './Components/Navbar/NavBar.jsx';
 import './style.css';
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
@@ -12,18 +12,15 @@ class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: 'React',
     };
   }
 
   render() {
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <>
+        <NavBar />
+      </>
     );
   }
 }
